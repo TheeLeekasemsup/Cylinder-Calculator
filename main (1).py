b299689f0bb13f75e7e1764bb6b6322ddf18b920
TheeLeekasemsup <86459752+TheeLeekasemsup@users.noreply.github.com>
2021-08-16 11:03:46 +0700

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,12 @@
+print("this is the calulator for the area and the volume of the calulator")
+print("Please enter the width")
+height = int(input(""))
+print("Please enter the radius")
+radius = int(input(""))
+print("the height is:",height)
+print("the radius is:",radius)
+print("the surface fo the cylinder is",(2 * 3.14159 * radius * height) + (2 * 3.14159 * radius *radius))
+print("the volume of the cylinder is:", 3.14159 * radius * radius * height)
+
+
+
